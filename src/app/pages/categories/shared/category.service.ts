b0e5575af1a2e3eff00921fd75ff071b0f130b5a
Ruhan Dorneles Borgmann <ruhan.borgmann@cwi.com.br>
2019-08-01 12:47:31 -0300

    category form

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -39,7 +39,7 @@ export class CategoryService {
 
   update(category: Category): Observable<Category>{
     const url = `${this.apiPath}/${category.id}`;
-    return this.http.put(url, category).pipe(
+    return this.http.put("sds"+url, category).pipe(
       catchError(this.handleError),
       map(() => category)
     )
